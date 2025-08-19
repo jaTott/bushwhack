e803b13315df5abb991ff4df332e67658f636eea
Jacques Tott-Buswell <130659910+jaTott@users.noreply.github.com>
2025-08-19 22:18:29 +0100

    Remove unused button asset and update input actions
    Deleted button.png and related import files from assets. Added new input actions (ui_left, ui_right, ui_up, ui_down, ui_boost) to project.godot for keyboard and gamepad support. Minor editor and scene configuration changes to reflect asset removal and input updates.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 @export var speed: float = 100.0
+@export var boost_factor: float = 2
+
 @export var player_color: Color
 @export var player_name: String
 
 var is_local: bool = false # only true for the character controlled
+var is_boosting: bool = false
 func _ready():
 	$Sprite2D.modulate = player_color
 	$Label.set_text(player_name)
@@ -17,9 +20,15 @@ func _physics_process(delta: float) -> void:
 		return # only control the local player
 		
 	var input_vector = Vector2.ZERO
+
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	velocity = input_vector.normalized() * speed
+	
+	if Input.get_action_strength("ui_boost"):
+		velocity = input_vector.normalized() * speed * boost_factor
+	else:
+		velocity = input_vector.normalized() * speed
+		
 	move_and_slide()
 	#print("working")
 	#rotate character
